5af124c0799eb55d01c9bf8254c37678b5abaaaf
ramnrupasinghe <ramnrupasinghe@students.nsbm.ac.lk>
2023-09-04 22:52:50 +0530

    calender popup update

--- lib/view/addevent_tickets.dart
+++ lib/view/addevent_tickets.dart
@@ -50,14 +50,15 @@ class _Addevent_ticketPageState extends State<Addevent_ticketPage> {
     }
   }
 
-    Future<void> _selectDate(BuildContext context) async {
-    final DateTime pickedDate = (await showDatePicker(
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? pickedDate = await showDatePicker(
       context: context,
       initialDate: DateTime.now(),
       firstDate: DateTime.now(),
       lastDate: DateTime(2101),
-    ))!;
-    if (pickedDate != DateTime.now()) {
+    );
+
+    if (pickedDate != null && pickedDate != DateTime.now()) {
       setState(() {
         dateController.text =
             "${pickedDate.day}/${pickedDate.month}/${pickedDate.year}";
@@ -78,174 +79,180 @@ class _Addevent_ticketPageState extends State<Addevent_ticketPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(kToolbarHeight),
-        child: Container(
-          color: const Color(0xFFD1E8D4),
-          child: AppBar(
-            backgroundColor: const Color(0xFFD1E8D4),
-            title: const Text('Add Event'),
+    return WillPopScope(
+      onWillPop: () async {
+        // Handle back button press here, if needed.
+        return true; // Set to true to allow back navigation, or false to block it.
+      },
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(kToolbarHeight),
+          child: Container(
+            color: const Color(0xFFD1E8D4),
+            child: AppBar(
+              backgroundColor: const Color(0xFFD1E8D4),
+              title: const Text('Add Event'),
+            ),
           ),
         ),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              color: const Color(0xFFBFC9C0),
-              child: SizedBox(
-                height: MediaQuery.of(context).size.height / 5,
-                child: InkWell(
-                  onTap: () {
-                    _pickImage();
-                  },
-                  child: Center(
-                    child: _selectedImage != null
-                        ? Image.file(_selectedImage!)
-                        : const Icon(
-                            Icons.add_a_photo_outlined,
-                            size: 30.0,
-                            color: Colors.grey,
-                          ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                color: const Color(0xFFBFC9C0),
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height / 5,
+                  child: InkWell(
+                    onTap: () {
+                      _pickImage();
+                    },
+                    child: Center(
+                      child: _selectedImage != null
+                          ? Image.file(_selectedImage!)
+                          : const Icon(
+                              Icons.add_a_photo_outlined,
+                              size: 30.0,
+                              color: Colors.grey,
+                            ),
+                    ),
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text('Event Title'),
-                  TextField(
-                    controller: eventController,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'Give a title to the event',
-                    ),
-                  ),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                           children: [
-                            const Text('Date'),
-                            TextField(
-                              controller: dateController,
-                              keyboardType: TextInputType.datetime,
-                              decoration: InputDecoration(
-                                border: OutlineInputBorder(),
-                                hintText: 'Enter the date',
-                                suffixIcon: IconButton(
-                                  icon: Icon(Icons.calendar_today),
-                                  onPressed: () {
-                                    _selectDate(context);
-                                  },
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 16,
-                      ),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children:  [
-                            const Text('Time'),
-                            TextField(
-                              controller: timeController,
-                              keyboardType: TextInputType.datetime,
-                              decoration: const InputDecoration(
-                                border: OutlineInputBorder(),
-                                hintText: 'Enter the time',
-                                suffixIcon: Icon(Icons.access_time),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                  const Text('Venue'),
-                  TextField(
-                    controller: venueController,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'Short sub-title to the event',
-                      suffixIcon: Icon(Icons.location_on_outlined),
-                    ),
-                  ),
-                  const Text('Description'),
-                  SizedBox(
-                    width: double.infinity,
-                    child: TextField(
-                      controller: descriptionController,
-                      maxLines: 4,
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text('Event Title'),
+                    TextField(
+                      controller: eventController,
+                      keyboardType: TextInputType.emailAddress,
                       decoration: const InputDecoration(
                         border: OutlineInputBorder(),
-                        hintText: '',
+                        hintText: 'Give a title to the event',
                       ),
                     ),
-                  ),
-                  const Text('External URL'),
-                  TextField(
-                    controller: urlController,
-                    keyboardType: TextInputType.url,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'Paste external URL',
-                      suffixIcon: Icon(Icons.link),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text('Date'),
+                              TextField(
+                                controller: dateController,
+                                keyboardType: TextInputType.datetime,
+                                decoration: InputDecoration(
+                                  border: const OutlineInputBorder(),
+                                  hintText: 'Enter the date',
+                                  suffixIcon: IconButton(
+                                    icon: const Icon(Icons.calendar_today),
+                                    onPressed: () {
+                                      _selectDate(context);
+                                    },
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 16,
+                        ),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text('Time'),
+                              TextField(
+                                controller: timeController,
+                                keyboardType: TextInputType.datetime,
+                                decoration: const InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  hintText: 'Enter the time',
+                                  suffixIcon: Icon(Icons.access_time),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                  const SizedBox(height: 16),
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        // Handle button press
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFF006D3E),
+                    const Text('Venue'),
+                    TextField(
+                      controller: venueController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Short sub-title to the event',
+                        suffixIcon: Icon(Icons.location_on_outlined),
                       ),
-                      child: const Text(
-                        'Publish As a free event',
-                        style: TextStyle(
-                          color: Colors.white,
+                    ),
+                    const Text('Description'),
+                    SizedBox(
+                      width: double.infinity,
+                      child: TextField(
+                        controller: descriptionController,
+                        maxLines: 4,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: '',
                         ),
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 8),
-                  const Center(
-                    child: Text('or'),
-                  ),
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        // Handle button press
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFFD1E8D4),
+                    const Text('External URL'),
+                    TextField(
+                      controller: urlController,
+                      keyboardType: TextInputType.url,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Paste external URL',
+                        suffixIcon: Icon(Icons.link),
                       ),
-                      child: const Text(
-                        'Add Tickets',
-                        style: TextStyle(
-                          color: Colors.black,
+                    ),
+                    const SizedBox(height: 16),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Handle button press
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFF006D3E),
+                        ),
+                        child: const Text(
+                          'Publish As a free event',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                    const SizedBox(height: 8),
+                    const Center(
+                      child: Text('or'),
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Handle button press
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFFD1E8D4),
+                        ),
+                        child: const Text(
+                          'Add Tickets',
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
