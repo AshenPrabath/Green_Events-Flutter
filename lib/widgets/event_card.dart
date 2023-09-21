18be8a05a80bd53cd830a2f7db662b1c38fe446c
Ashen Prabath <ashenprabath31@gmail.com)>
2023-09-21 14:07:55 +0530

    Created UserCard widget

---
+++ lib/widgets/event_card.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+
+import '../models/event_model.dart';
+import '../services/event_service.dart';
+
+class EventCard extends StatefulWidget {
+  final Event event;
+  const EventCard({
+    Key? key,
+    required this.event,
+  }) : super(key: key);
+
+  @override
+  State<EventCard> createState() => _EventCardState();
+}
+
+class _EventCardState extends State<EventCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        elevation: 0,
+        shape: RoundedRectangleBorder(
+          side: BorderSide(
+            color: Theme.of(context).colorScheme.outline,
+          ),
+          borderRadius: const BorderRadius.all(Radius.circular(12)),
+        ),
+        child: Column(
+          children: [
+            ListTile(
+              trailing: GestureDetector(
+                  onTap: () {}, child: const Icon(Icons.star_border)),
+              leading: Image.asset(
+                'lib/assets/background.png',
+                width: 40,
+                height: 40,
+              ),
+              title: Text(
+                widget.event.title,
+                style: Theme.of(context)
+                    .textTheme
+                    .titleMedium
+                    ?.copyWith(color: Theme.of(context).colorScheme.onSurface),
+              ),
+              subtitle: Row(
+                children: [
+                  Text(
+                    '${widget.event.userId} |',
+                    style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                        color: Theme.of(context).colorScheme.onSurfaceVariant),
+                  ),
+                  Text(
+                    ' ${widget.event.time}',
+                    style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 16),
+              child: Image.asset(
+                'lib/assets/media.png',
+                height: 152,
+                width: 356,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(widget.event.title,
+                      style: Theme.of(context).textTheme.headlineMedium),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "📅 Date : 20th July 2023",
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                            color:
+                                Theme.of(context).colorScheme.onSurfaceVariant),
+                      ),
+                      Text(
+                        "⏰ Time : 9.00 am",
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                            color:
+                                Theme.of(context).colorScheme.onSurfaceVariant),
+                      ),
+                      Text(
+                        "📍 Venue : ${widget.event.venue}",
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                            color:
+                                Theme.of(context).colorScheme.onSurfaceVariant),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Text(
+                widget.event.desc,
+                style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                    color: Theme.of(context).colorScheme.onSurfaceVariant),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text('Free Registration '),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: FilledButton(
+                    onPressed: () {},
+                    child: const Text('Register'),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
