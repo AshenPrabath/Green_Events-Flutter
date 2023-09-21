f63708bd8ff447efa2df54eb8c69e1f11309b8cd
ramnrupasinghe <ramnrupasinghe@students.nsbm.ac.lk>
2023-09-21 16:24:17 +0530

    Created add Event & add tickets pages

---
+++ lib/pages/add_event_page.dart
@@ -0,0 +1,183 @@
+import 'package:flutter/material.dart';
+
+import '../pages/add_tickets.dart';
+import '../Services/event_service.dart';
+import '../Services/user_service.dart';
+import '../widgets/custom_filled_button.dart';
+import '../widgets/date_picker_textfield.dart';
+import '../widgets/input_large_textfield.dart';
+import '../widgets/input_textfield.dart';
+import '../widgets/time_picker_textfield.dart';
+
+class AddEventPage extends StatefulWidget {
+  const AddEventPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<AddEventPage> createState() => _AddEventPageState();
+}
+
+class _AddEventPageState extends State<AddEventPage> {
+  final _formKey = GlobalKey<FormState>();
+  String eventUrl = "";
+  String eventTitle = '';
+  late TimeOfDay eventTime;
+  late DateTime eventDate;
+  String eventVenue = '';
+  final TextEditingController _descText = TextEditingController();
+  String? eventLink;
+
+  String buttonText = "Publish as a free event";
+
+  int get combinedDateTime => DateTime(
+        eventDate.year,
+        eventDate.month,
+        eventDate.day,
+        eventTime.hour,
+        eventTime.minute,
+      ).millisecondsSinceEpoch;
+
+  @override
+  void dispose() {
+    _descText.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
+        title: Text(
+          "Add Event",
+          style: Theme.of(context)
+              .textTheme
+              .titleLarge
+              ?.copyWith(color: Theme.of(context).colorScheme.onSurface),
+        ),
+      ),
+      body: Form(
+        key: _formKey,
+        child: ListView(
+          children: [
+            Image.asset('lib/assets/Group 9.png'),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: OverflowBar(
+                  overflowSpacing: 15,
+                  children: [
+                    const SizedBox(),
+                    InputTextField(
+                      labelText: "Event Title",
+                      hint: "Give a title to the event",
+                      isPassword: false,
+                      onChanged: (text) {
+                        eventTitle = text;
+                      },
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        TimePickerTextField(
+                          hint: "TIme",
+                          onTimePicked: (pickedTime) {
+                            eventTime = pickedTime;
+                          },
+                        ),
+                        const Spacer(),
+                        DatePickerTextField(
+                          hint: "Date",
+                          onDatePicked: (pickedDate) {
+                            eventDate = pickedDate;
+                          },
+                        )
+                      ],
+                    ),
+                    InputTextField(
+                      labelText: "Venue",
+                      hint: "Enter the location ",
+                      suffixIcon: Icons.location_on_outlined,
+                      onChanged: (value) {
+                        eventVenue = value;
+                      },
+                    ),
+                    InputLargeTextField(
+                      controller: _descText,
+                      maxLength: 250,
+                    ),
+                    InputTextField(
+                      isOptional: true,
+                      labelText: "External Link",
+                      hint: "Paste if you have any external Links.",
+                      helperText: "*Optional",
+                      suffixIcon: Icons.link,
+                      onChanged: (value) {
+                        eventLink = value;
+                      },
+                    ),
+                    OverflowBar(
+                      overflowSpacing: 7,
+                      children: [
+                        CustomFilledButton(
+                            onPressed: () async {
+                              if (_formKey.currentState!.validate()) {
+                                await EventService.addEvent(
+                                  UserService.getUserId(),
+                                  eventTitle,
+                                  eventUrl,
+                                  combinedDateTime,
+                                  eventVenue,
+                                  _descText.text.toString(),
+                                  true,
+                                  eventLink,
+                                );
+                              }
+                            },
+                            buttonText: buttonText),
+                        Center(
+                          child: Text(
+                            "or",
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyLarge
+                                ?.copyWith(
+                                    color: Theme.of(context)
+                                        .colorScheme
+                                        .onBackground),
+                          ),
+                        ),
+                        CustomFilledButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => AddTicket(
+                                    // event: widget.events,
+                                    ),
+                              ),
+                            );
+                          },
+                          buttonText: "Add Tickets",
+                          buttonColor:
+                              Theme.of(context).colorScheme.secondaryContainer,
+                          buttonTextColor: Theme.of(context)
+                              .colorScheme
+                              .onSecondaryContainer,
+                        ),
+                        const SizedBox(
+                          height: 50,
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/add_tickets.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+import '../models/ticket_model.dart';
+import '../source/ticket_data.dart';
+import '../widgets/add_tickets_dialog.dart';
+import '../widgets/ticket_card.dart';
+
+class AddTicket extends StatefulWidget {
+  // final Event event;
+  const AddTicket({
+    Key? key,
+    // required this.event,
+  }) : super(key: key);
+
+  @override
+  State<AddTicket> createState() => _AddTicketState();
+}
+
+class _AddTicketState extends State<AddTicket> {
+  void _addTicket(Ticket newTicket) {
+    setState(() {
+      ticketsList.add(newTicket); // Add the ticket to the ticketsList
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
+        title: Text(
+          "Add tickets",
+          style: Theme.of(context)
+              .textTheme
+              .titleLarge
+              ?.copyWith(color: Theme.of(context).colorScheme.onSurface),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              "Publish",
+              style: Theme.of(context)
+                  .textTheme
+                  .titleMedium
+                  ?.copyWith(color: Theme.of(context).colorScheme.primary),
+            ),
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: ticketsList.length, // Use ticketsList here
+        itemBuilder: (context, index) {
+          return TicketCard(
+            // event: widget.event,
+            ticket: ticketsList[index],
+          ); // Use ticketsList here
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AddTicketDialog(
+                onTicketAdded: _addTicket,
+              );
+            },
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
