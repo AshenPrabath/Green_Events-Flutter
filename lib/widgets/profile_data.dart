699b56a8e040e090960fd1084d814e381a8103ac
Ashen Prabath <ashenprabath31@gmail.com)>
2023-09-21 14:08:40 +0530

    created profileData widget

---
+++ lib/widgets/profile_data.dart
@@ -0,0 +1,181 @@
+import 'package:flutter/material.dart';
+import '../models/user_model.dart';
+import '../pages/add_organization_page.dart';
+import '../Pages/settings_page.dart';
+
+class ProfileData extends StatelessWidget {
+  final User user;
+  const ProfileData({
+    Key? key,
+    required this.user,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              child: Image.asset(
+                "lib/assets/3d_avatar_18.png",
+                width: 105,
+                height: 105,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 17),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    user.name,
+                    style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                        color: Theme.of(context).colorScheme.onBackground),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: Text(
+                      user.email,
+                      style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                          color: Theme.of(context).colorScheme.onBackground),
+                    ),
+                  ),
+                  GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const SettingsPage()),
+                        );
+                      },
+                      child: const Icon(Icons.settings_outlined)),
+                ],
+              ),
+            )
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 31),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Personal info",
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge
+                    ?.copyWith(color: Theme.of(context).colorScheme.primary),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Icon(
+                  Icons.edit_outlined,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 25),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Phone Number",
+                style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground),
+              ),
+              Text(
+                user.phone,
+                style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 25),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Faculty",
+                style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground),
+              ),
+              Text(
+                user.faculty,
+                style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 25),
+          child: Text(
+            "Organizations",
+            style: Theme.of(context)
+                .textTheme
+                .titleLarge
+                ?.copyWith(color: Theme.of(context).colorScheme.primary),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 30),
+          child: SizedBox(
+            width: double.infinity,
+            // child: OutlinedButton(
+            //   style: OutlinedButton.styleFrom(
+            //     shape: RoundedRectangleBorder(
+            //       borderRadius: BorderRadius.circular(12.0),
+            //     ),
+            //     side: BorderSide(
+            //         width: 1,
+            //         color: Theme.of(context).colorScheme.outlineVariant),
+            //   ),
+            //   onPressed: () {},
+            //   child: Padding(
+            //     padding: EdgeInsets.symmetric(vertical: 15),
+            //     child: Text(
+            //       "Add an Organization",
+            //       style: Theme.of(context).textTheme.titleMedium?.copyWith(
+            //           color: Theme.of(context).colorScheme.onSurface),
+            //     ),
+            //   ),
+            // ),
+            child: OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                side: BorderSide(
+                    width: 1,
+                    color: Theme.of(context).colorScheme.outlineVariant),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const AddOrganizationPage()),
+                );
+              },
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 15),
+                child: Text(
+                  "Add an Organization",
+                  style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                      color: Theme.of(context).colorScheme.onSurface),
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
